4afff5c392fe2fc163e7be3b107974cc194392e7
KaierTseng <jyp@livemail.tw>
2017-04-21 11:39:25 +0800

    Add project page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 // app.get('/about', (req, res) => {
 //     res.send('About page');
 // });
